051ac9d58a05fd6edd3d827c7dec5d3e5fef8dd3
ajeetcoolkarni <ajeetcoolkarni@gmail.com>
2018-05-02 14:24:21 +0530

    Updated

--- readme.txt
+++ readme.txt
@@ -2,3 +2,4 @@ ModuleLoaders
 Plugins
 Plyfills
 Transpilers
+Boilerplates : ionic, create-react-app,create-angular-ionic-app,create-react-ts-app
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+import  '../css/styles.css'
+
 import {button,header} from './fetchSelector'
 
 button.addEventListener('click',onToggleButtonClick);
